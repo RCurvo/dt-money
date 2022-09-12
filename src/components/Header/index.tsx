0606bae7b47141c6478db0c9619046feb0c97f37
RCurvo <renan.curvo@gmail.com>
2022-09-12 14:43:37 -0300

    Implements modal with radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import LogoImg from '../../assets/Logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={LogoImg} alt=""></img>
-        <NewTransactionButton> Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton> Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
